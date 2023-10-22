afb3dae162428c4eb9221707a98606afc6ba295e
Ryztard <72693058+Ryztard@users.noreply.github.com>
2023-10-22 22:48:03 +0200

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,6 @@
 import Link from "next/link"
 
 const Navigation = () => {
-//Router gir error, har prøvd mange forskjellige versjoner av navigasjoner + prøvd å brukt TSX filer istedet. 
-//Prøvd å følge undervisnings videon også men noe slår seg feil, men vet ikke hva som går galt. 
     return (
       <nav>
         <ul>
